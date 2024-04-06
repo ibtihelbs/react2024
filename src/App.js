065c3e1629fec6ba4a903b5e41c8b4efee839a7b
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-04-06 10:22:07 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import "./App.css";
-import Card from "./componants/Card";
-import Products from "./componants/Products";
-import TodoApp from "./componants/TodoApp";
 
 function App() {
   return (
     <div className="App">
-      <TodoApp />
+      <div class="center">
+        <header class="flex between">
+          <h1>Todo</h1>
+          <button class="theme-switcher">
+            <img src="./images/icon-sun.svg" alt="icon-sun" />
+          </button>
+        </header>
+        <main>
+          <form id="todo-form" class="flex">
+            <span></span>
+            <input type="text" id="add" placeholder="Create new todo ...." />
+          </form>
+          <section id="tasks-management">
+            <div id="tasks-wrap">
+              <div class="single-task flex between">
+                <div class="flex">
+                  <form class="checked">
+                    <input type="checkbox" class="state" />
+                  </form>
+                  <p>new task</p>
+                </div>
+                <button class="delete">
+                  <img src="./images/icon-cross.svg" alt="icon-cross" />
+                </button>
+              </div>
+            </div>
+            <hr />
+            <div class="filters flex between">
+              <div>
+                <span id="active-number">0</span>
+                <span>items left</span>
+              </div>
+              <div class="filter-btn">
+                <button class="all">All</button>
+                <button class="active">Active</button>
+                <button class="completed">Completed</button>
+              </div>
+              <button class="clear-completed">Clear Completed</button>
+            </div>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
